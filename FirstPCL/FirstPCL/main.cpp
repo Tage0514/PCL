da65b20229d068150597d99c750833df3c015fdf
Tage0514 <33489648+Tage0514@users.noreply.github.com>
2018-07-10 11:10:31 +0800

    change

--- FirstPCL/FirstPCL/main.cpp
+++ FirstPCL/FirstPCL/main.cpp
@@ -1,3 +1,27 @@
+/*
+*
+* ━━━━━━神兽出没━━━━━━
+* 　　　┏┓　　　┏┓
+* 　　┏┛┻━━━┛┻┓
+* 　　┃　　　　　　　┃
+* 　　┃　　　━　　　┃
+* 　　┃　┳┛　┗┳　┃
+* 　　┃　　　　　　　┃
+* 　　┃　　　┻　　　┃
+* 　　┃　　　　　　　┃
+* 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+* 　　　　┃　　　┃    神兽保佑,代码无bug
+* 　　　　┃　　　┃
+* 　　　　┃　　　┗━━━┓
+* 　　　　┃　　　　　　　┣┓
+* 　　　　┃　　　　　　　┏┛
+* 　　　　┗┓┓┏━┳┓┏┛
+* 　　　　　┃┫┫　┃┫┫
+* 　　　　　┗┻┛　┗┻┛
+*
+* ━━━━━━感觉萌萌哒━━━━━━
+*/
+
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
@@ -85,73 +109,74 @@ int main()
 
 		/*std::cerr << "Cloud before filtering: " << std::endl;
 		for (size_t i = 0; i < cloud->points.size(); ++i)
-			std::cerr << "    " << cloud->points[i].x << " "
-			<< cloud->points[i].y << " "
-			<< cloud->points[i].z << std::endl;*/
+		std::cerr << "    " << cloud->points[i].x << " "
+		<< cloud->points[i].y << " "
+		<< cloud->points[i].z << std::endl;*/
 
 
 		//blocks until the cloud is actually rendered
 
-		viewer.showCloud(cloud);
-		//////////////////// Normal estimation*
-		//////////////////pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
-		//////////////////pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
-		//////////////////pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
-		//////////////////tree->setInputCloud(cloud);
-		//////////////////n.setInputCloud(cloud);
-		//////////////////n.setSearchMethod(tree);
-		//////////////////n.setKSearch(20);
-		//////////////////n.compute(*normals);
-		////////////////////* normals should not contain the point normals + surface curvatures
+		//viewer.showCloud(cloud);
+		// Normal estimation*
+		pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+		pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+		pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+		tree->setInputCloud(cloud);
+		n.setInputCloud(cloud);
+		n.setSearchMethod(tree);
+		n.setKSearch(20);
+		n.compute(*normals);
+		//* normals should not contain the point normals + surface curvatures
 
-		//////////////////// Concatenate the XYZ and normal fields*
-		//////////////////pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>);
-		//////////////////pcl::concatenateFields(*cloud, *normals, *cloud_with_normals);
-		////////////////////* cloud_with_normals = cloud + normals
+		// Concatenate the XYZ and normal fields*
+		pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>);
+		pcl::concatenateFields(*cloud, *normals, *cloud_with_normals);
+		//* cloud_with_normals = cloud + normals
 
-		//////////////////// Create search tree*
-		//////////////////pcl::search::KdTree<pcl::PointNormal>::Ptr tree2(new pcl::search::KdTree<pcl::PointNormal>);
-		//////////////////tree2->setInputCloud(cloud_with_normals);
+		// Create search tree*
+		pcl::search::KdTree<pcl::PointNormal>::Ptr tree2(new pcl::search::KdTree<pcl::PointNormal>);
+		tree2->setInputCloud(cloud_with_normals);
 
-		//////////////////// Initialize objects
-		//////////////////pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
-		//////////////////pcl::PolygonMesh triangles;
+		// Initialize objects
+		pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+		pcl::PolygonMesh triangles;
 
-		//////////////////// Set the maximum distance between connected points (maximum edge length)
-		//////////////////gp3.setSearchRadius(0.025);
+		// Set the maximum distance between connected points (maximum edge length)
+		gp3.setSearchRadius(0.1);
 
-		//////////////////// Set typical values for the parameters
-		//////////////////gp3.setMu(2.5);
-		//////////////////gp3.setMaximumNearestNeighbors(100);
-		//////////////////gp3.setMaximumSurfaceAngle(M_PI / 4); // 45 degrees
-		//////////////////gp3.setMinimumAngle(M_PI / 18); // 10 degrees
-		//////////////////gp3.setMaximumAngle(2 * M_PI / 3); // 120 degrees
-		//////////////////gp3.setNormalConsistency(false);
+		// Set typical values for the parameters
+		gp3.setMu(2.5);
+		gp3.setMaximumNearestNeighbors(100);
+		gp3.setMaximumSurfaceAngle(M_PI / 4); // 45 degrees
+		gp3.setMinimumAngle(M_PI / 18); // 10 degrees
+		gp3.setMaximumAngle(2 * M_PI / 3); // 120 degrees
+		gp3.setNormalConsistency(false);
 
-		//////////////////// Get result
-		//////////////////gp3.setInputCloud(cloud_with_normals);
-		//////////////////gp3.setSearchMethod(tree2);
-		//////////////////gp3.reconstruct(triangles);
+		// Get result
+		gp3.setInputCloud(cloud_with_normals);
+		gp3.setSearchMethod(tree2);
+		gp3.reconstruct(triangles);
 
-		//////////////////// Additional vertex information
-		//////////////////std::vector<int> parts = gp3.getPartIDs();
-		//////////////////std::vector<int> states = gp3.getPointStates();
+		// Additional vertex information
+		std::vector<int> parts = gp3.getPartIDs();
+		std::vector<int> states = gp3.getPointStates();
 
-		//////////////////boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
-		//////////////////viewer->setBackgroundColor(0, 0, 0);
+		boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+		viewer->setBackgroundColor(0, 0, 0);
 
-		//////////////////viewer->addPolygonMesh(triangles, "triangles");
+		viewer->addPolygonMesh(triangles, "triangles");
 
-		//////////////////viewer->addCoordinateSystem(1.0);
-		//////////////////viewer->initCameraParameters();
-		//////////////////while (!viewer->wasStopped()) {
-		//////////////////	viewer->spinOnce(100);
-		//////////////////	boost::this_thread::sleep(boost::posix_time::microseconds(100000));
-		//////////////////}
+		viewer->addCoordinateSystem(1.0);
+		viewer->initCameraParameters();
+		while (!viewer->wasStopped()) {
+			viewer->spinOnce(100);
+			boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+		}
 
-	
+
+
 	}
-	
+
 	/*use the following functions to get access to the underlying more advanced/powerful
 	PCLVisualizer
 	This will only get called once
@@ -169,4 +194,6 @@ int main()
 	//}
 	return 0;
 }
+
+
 
